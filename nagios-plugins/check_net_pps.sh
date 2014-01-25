d18b068c81dbee6bb81d17a9b352bd17b9ecc9a3
Dominik <dominik2k1x@gmail.com>
2014-01-25 19:46:14 +0100

    Some beauty things

--- nagios-plugins/check_net_pps.sh
+++ nagios-plugins/check_net_pps.sh
@@ -7,9 +7,9 @@ LIMIT_TX=$3
 LIMIT_RX=$4
 
 if ([ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ] || [ -z "$4" ]); then
-        echo Usage: $0 [Interval] [Interface] [Limit TX Pkts/s] [Limit RX Pkts/s]
-        echo Example: $0 1 eth0 8000 8000
-        exit 1
+    echo Usage: $0 [Interval Seconds] [Interface] [Threshold TX Pkts/s] [Threshold RX Pkts/s]
+    echo Example: $0 1 eth0 8000 8000
+    exit 1
 fi
 
 R1=`cat /sys/class/net/$INTERFACE/statistics/rx_packets`
@@ -27,13 +27,13 @@ CALC_TX=`echo "$TX_PPS > $LIMIT_TX" | bc`
 CALC_RX=`echo "$RX_PPS > $LIMIT_RX" | bc`
 
 if [ $CALC_TX == 1 ] ; then
-        echo "CRITICAL - Network Out: $TX_PPS Pkts/s"
-        exit 2
+    echo "CRITICAL - Network Out: $TX_PPS Pkts/s"
+    exit 2
 fi
 
 if [ $CALC_RX == 1 ] ; then
-        echo "CRITICAL - Network In: $RX_PPS Pkts/s"
-        exit 2
+    echo "CRITICAL - Network In: $RX_PPS Pkts/s"
+    exit 2
 fi
 
 echo "OK - OUT: $TX_PPS Pkts/s // IN: $RX_PPS Pkts/s"
